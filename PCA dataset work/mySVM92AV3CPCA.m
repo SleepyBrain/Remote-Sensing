9e3e585aed9f0e5d312f1b784d3aae206cf13b52
Tanvir Ahmed <tanvirahmed14@gmail.com>
2017-11-13 21:03:59 +0600

    Adding thesis book

--- PCA dataset work/mySVM92AV3CPCA.m
+++ PCA dataset work/mySVM92AV3CPCA.m
@@ -4,21 +4,31 @@
 addpath('F:\CSE 700&800\Data set\10_4231_R7RX991C\aviris_hyperspectral_data');
 resultPCA = [];
 
-load AVIRISPCA_train.txt;
-train = AVIRISPCA_train;
-clear AVIRISPCA_train;
+% load AVIRISPCA_train.txt;
+% train = AVIRISPCA_train;
+% clear AVIRISPCA_train;
+
+load NewTrain.txt;
+train = NewTrain;
+clear NewTrain;
+
 label_train = train(:,1);
 train(:,1:2)=[];
-train = train(:,1:220);
-% train = train(:,id);
+% train = train(:,1:220);
+train = train(:,id);
 
-load AVIRISPCA_test.txt;
-test = AVIRISPCA_test;
-clear AVIRISPCA_test;
+% load AVIRISPCA_test.txt;
+% test = AVIRISPCA_test;
+% clear AVIRISPCA_test;
+
+load NewTest.txt;
+test = NewTest;
+clear NewTest;
+
 label_test = test(:,1);
 test(:,1:2)=[];
-train = train(:,1:220);
-% test = test(:,id);
+% train = train(:,1:220);
+test = test(:,id);
 
 for i=1:size(train, 2)
     train(:,i)=scaledata(train(:,i));
@@ -27,24 +37,24 @@ end
 
 addpath('F:\CSE 700&800\Data set\libsvm-3.22\matlab');
 
-bestc=10; bestg=0.83;
+% bestc=10; bestg=0.83;
 % bestc=10; bestg=0.28;
 
-%     bestcv=0; bestc=0; bestg=0;
-%     for c = 1:10
-%         for g = 0.01:0.01:5
-%             cmd=['-v 10 -c ',num2str(c), ' -g ', num2str(g)];
-%             cv = svmtrain(label_train, train, cmd);
-%             if(cv>=bestcv)
-%                 bestcv=cv; bestc=c; bestg=g;
-%             end
-%             fprintf('%g   %g  %g (best c=%g, g=%g, rate=%g)\n', c, g, cv, bestc, bestg, bestcv);
-%         end
-%     end
+    bestcv=0; bestc=0; bestg=0;
+    for c = 1:10
+        for g = 0.01:0.01:5
+            cmd=['-v 5 -c ',num2str(c), ' -g ', num2str(g)];
+            cv = svmtrain(label_train, train, cmd);
+            if(cv>=bestcv)
+                bestcv=cv; bestc=c; bestg=g;
+            end
+            fprintf('%g   %g  %g (best c=%g, g=%g, rate=%g)\n', c, g, cv, bestc, bestg, bestcv);
+        end
+    end
 
 cmd=['-t 2 -c ',num2str(bestc), ' -g ', num2str(bestg)];
 
-for f=1:10
+for f=1:size(id,2)
     model = svmtrain(label_train,train(:,1:f),cmd);
     [predict_label, accuracy, dec_values] = svmpredict(label_test, test(:,1:f), model);
     resultPCA = [resultPCA accuracy(1)];
---
+++ Thesis Book/README.md
@@ -0,0 +1,31 @@
+# RUET Thesis Template LaTeX
+
+![Cover](doc/cover.png)
+
+A boilerplate LaTeX template for writing B.Sc. Undergraduate Thesis under at RUET.
+
+## Features
+
+* Uses latex `subfiles` module to load title page, acknowledgement page, certificate page, abstract page, chapters, and appendix from separate `tex` files
+* IEEE styled bibliography from separate `bib` file
+
+## Examples
+
+* Demo chapters, sections, and subsections
+* Demo single figures, multiple figures and captions
+* Demo list items
+* Demo enumerated items
+* Demo tables
+* Demo equations
+
+## System Requirment
+
+Install TeXstudio to compile and view the LaTex project.
+
+* Linux: `apt-get install -y lyx texstudio texlive-extra-utils`
+* OS X: `brew cask install texstudio`
+* Windows: Download from `http://sourceforge.net/projects/texstudio/files/texstudio/`
+
+## Preview
+
+Download `document.pdf` and open with any PDF viewer or [click here](PREVIEW.md) to see the page previews.
