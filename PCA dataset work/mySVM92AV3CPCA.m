30ab5d55a2cc9cf6d6865a3a68802363803f8f82
Tanvir Ahmed <tanvirahmed14@gmail.com>
2017-11-23 03:32:20 +0600

    modified

--- PCA dataset work/mySVM92AV3CPCA.m
+++ PCA dataset work/mySVM92AV3CPCA.m
@@ -38,22 +38,20 @@ end
 
 addpath('F:\CSE 700&800\Data set\libsvm-3.22\matlab');
 
-% bestc=10; bestg = 0.83;
-% bestc=10; bestg = 0.28;
-% bestc=10; bestg = 1.53;
-% bestc=11; bestg=3.5;
+% bestc=10; bestg = 6;
+bestc=9; bestg = 5.72;
 
-    bestcv=0; bestc=0; bestg=0;
-    for c = 1:20
-        for g = 0:0.1:10
-            cmd=['-v 5 -c ',num2str(c), ' -g ', num2str(g)];
-            cv = svmtrain(label_train, train, cmd);
-            if(cv>=bestcv)
-                bestcv=cv; bestc=c; bestg=g;
-            end
-            fprintf('%g   %g  %g (best c=%g, g=%g, rate=%g)\n', c, g, cv, bestc, bestg, bestcv);
-        end
-    end
+%     bestcv=0; bestc=0; bestg=0;
+%     for c = 8:12
+%         for g = 5:0.01:7
+%             cmd=['-v 10 -c ',num2str(c), ' -g ', num2str(g)];
+%             cv = svmtrain(label_train, train, cmd);
+%             if(cv>=bestcv)
+%                 bestcv=cv; bestc=c; bestg=g;
+%             end
+% %             fprintf('%g   %g  %g (best c=%g, g=%g, rate=%g)\n', c, g, cv, bestc, bestg, bestcv);
+%         end
+%     end
 
 cmd=['-t 2 -c ',num2str(bestc), ' -g ', num2str(bestg)];
 
