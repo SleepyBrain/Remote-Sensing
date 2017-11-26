b128d3c127e88ab25932189857ef5331718c2865
Tanvir Ahmed <tanvirahmed14@gmail.com>
2017-11-26 22:22:58 +0600

    Modification

--- PCA dataset work/mySVM92AV3CPCA.m
+++ PCA dataset work/mySVM92AV3CPCA.m
@@ -10,7 +10,7 @@ clear AVIRISPCA_train;
 
 label_train = train(:,1);
 train(:,1:2)=[];
-train = train(:,1:220);
+train = train(:,1:10);
 % train = train(:,id);
 
 load AVIRISPCA_test.txt;
@@ -19,7 +19,7 @@ clear AVIRISPCA_test;
 
 label_test = test(:,1);
 test(:,1:2)=[];
-train = train(:,1:220);
+train = train(:,1:10);
 % test = test(:,id);
 
 for i=1:size(train, 2)
@@ -29,12 +29,26 @@ end
 
 addpath('F:\CSE 700&800\Data set\libsvm-3.22\matlab');
 
-bestc=9; bestg = 5.72;
+% bestc=9; bestg = 5.72;
 % bestc=10; bestg = 6;
+bestc=19; bestg = 2.4; %last time PCA
+% bestc=16; bestg = 0.7; %last time PCA+NMI
+
+% bestcv=0; bestc=0; bestg=0;
+% for c = 1:20
+%     for g = 0:0.1:10
+%         cmd=['-v 10 -c ',num2str(c), ' -g ', num2str(g)];
+%         cv = svmtrain(label_train, train, cmd);
+%         if(cv>=bestcv)
+%             bestcv=cv; bestc=c; bestg=g;
+%         end
+% %         fprintf('%g   %g  %g (best c=%g, g=%g, rate=%g)\n', c, g, cv, bestc, bestg, bestcv);
+%     end
+% end
 
 cmd=['-t 2 -c ',num2str(bestc), ' -g ', num2str(bestg)];
 
-for f=1:size(id,2)
+for f=1:10
     model = svmtrain(label_train,train(:,1:f),cmd);
     [predict_label, accuracy, dec_values] = svmpredict(label_test, test(:,1:f), model);
     resultPCA = [resultPCA accuracy(1)];
