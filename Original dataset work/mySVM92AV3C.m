53de78df1705347c43663b73b18df83321b0aa48
Tanvir Ahmed <tanvirahmed14@gmail.com>
2017-12-04 00:07:34 +0600

    Modification

--- Original dataset work/mySVM92AV3C.m
+++ Original dataset work/mySVM92AV3C.m
@@ -10,8 +10,8 @@ clear AVIRIS_train;
 
 label_train = train(:,1);
 train(:,1:2)=[];
-% train = train(:,1:8);
-train = train(:,S(1:10));
+train = train;
+% train = train(:,S(1:10));
 
 
 %test data
@@ -21,8 +21,8 @@ clear AVIRIS_test;
 
 label_test = test(:,1);
 test(:,1:2)=[];
-% test = test(:,1:8);
-test = test(:,S(1:10));
+test = test;
+% test = test(:,S(1:10));
 
 
 
@@ -49,7 +49,7 @@ bestc=10;bestg=2.44;%best Org+NMI
 % end
 
 cmd=['-t 2 -c ',num2str(bestc), ' -g ', num2str(bestg)];
-for f=1:10
+for f=1:220
     model = svmtrain(label_train,train(:,1:f),cmd);
     [predict_label, accuracy, dec_values] = svmpredict(label_test, test(:,1:f), model);
     resultOrg = [resultOrg accuracy(1)];
